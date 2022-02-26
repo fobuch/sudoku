83a469a540d38183805f1d62c732d408a5f24d53
mandarancza <tenisremis@gmail.com>
2022-02-26 15:19:08 +0100

    added background color changing & input from numpad

--- main.js
+++ main.js
@@ -1,9 +1,82 @@
+let board = createBoard();
+//console.log(howManyDivides(9,9));
+
 const cellMini = document.querySelectorAll('.cell-mini');
+const numpad = document.querySelectorAll('.numpad');
+
+let activeCell = 40;
+activeCellBackground(activeCell, true);
 
 cellMini.forEach(cell => {
-    cell.addEventListener('click', function handleClick(event) {
-        console.log('cell clicked', event);
-    
-        cell.setAttribute('style', 'background-color: yellow;');
+    cell.addEventListener('click', () => {
+        let id = parseInt(cell.id.slice(1));
+        //document.getElementById('c'+activeCell).style.backgroundColor = '';
+        activeCellBackground(activeCell, false);
+        activeCell = id;
+        activeCellBackground(activeCell, true);
       });
 });
+
+numpad.forEach(num => {
+    num.addEventListener('click', function handleClick(event) {
+        let numId = parseInt(num.id.slice(11));
+        document.getElementById('c' + activeCell).innerText = numId;
+        board[activeCell] = numId;
+    });
+});
+
+function createBoard(){
+    let returnBoard = [];
+    for(let i = 0; i < 81; i++){
+        returnBoard[i] = 0;
+    }
+    return returnBoard;
+}
+
+function activeCellBackground(cellId, option){
+    let color = '';
+    if(option) color = 'lightgray';
+
+    let cellIdCopy = cellId;
+    while(cellIdCopy <= 80){
+        document.getElementById('c'+cellIdCopy).style.backgroundColor = color;
+        cellIdCopy += 9;
+    }
+
+    cellIdCopy = cellId;
+    while(cellIdCopy >= 0){
+        document.getElementById('c'+cellIdCopy).style.backgroundColor = color;
+        cellIdCopy -= 9;
+    }
+
+    for(let i = cellId - (cellId % 9); i <= cellId - (cellId % 9) + 8; i++){
+        document.getElementById('c'+i).style.backgroundColor = color;
+    }
+
+    for(let j = cellId - (howManyDivides(cellId,9) % 3) * 9; j < (cellId - (howManyDivides(cellId,9) % 3) * 9) + 27; j += 9){
+        for(let i = j - (j % 3); i < j - (j % 3) +3; i++){
+            document.getElementById('c'+i).style.backgroundColor = color;
+            console.log(i);
+        }
+    }
+
+    if(option){
+        document.getElementById('c' + cellId).style.backgroundColor = 'gray';
+    }else{
+        document.getElementById('c' + cellId).style.backgroundColor = '';
+    }
+    
+    
+}
+
+function howManyDivides(number, divider){
+    let counter = 0;
+    let outcome = number - divider;
+
+    while(outcome >= 0){
+        counter++;
+        outcome -= divider;
+    }
+
+    return counter;
+}
--- writeBoard.js
+++ writeBoard.js
@@ -1,5 +1,6 @@
 let code = "";
-
+let temp = 0;
+let temp2 = 0;
 for(let i = 1; i < 4; i++){
     code += `<tr class="line" id="line${i}">`;
     for(let j = 1; j < 4; j++){
@@ -7,14 +8,18 @@ for(let i = 1; i < 4; i++){
         code += '<table class="table-mini">';
         for(let k = 1; k < 4; k++){
             code += `<tr class="line-mini" id="line-mini${k}">`;
-            for(let l = 1; l < 4; l++){
-                code += `<td class="cell-mini" id="cell-mini${l}"></td>`
+            for(let l = 0; l < 3; l++){
+                code += `<td class="cell-mini" id="c${l+temp2+temp}"></td>`
             }
+            temp2 += 9;
             code += '</tr>';
         }
+        temp += 3;
+        temp2 = 0;
         code += '</table>';
         code += '</td>';
     }
+    temp += 18;
     code += '</tr>'
 }
 
